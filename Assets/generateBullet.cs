62d4408bc73051ba6de7f6c396b9400a9fb58056
DonghyunPark <d0nghyunp4rk@gmail.com>
2023-05-02 21:52:00 +0900

    gamecontroller

--- Assets/generateBullet.cs
+++ Assets/generateBullet.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class generateBullet : MonoBehaviour
 {
     public int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
+        InvokeRepeating("bullet", 1, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+ 
+    }
+
+    void bullet()
+    {
+        Instantiate(bullet);
     }
 
     public void getScore()
